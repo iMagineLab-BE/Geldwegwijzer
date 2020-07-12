3766d2f2c1a8b50d7564280125a8a2b089acc75a
Thomas Huybrechts <thomas.huybrechts@uantwerpen.be>
2020-07-13 01:10:50 +0200

    Added on destroy method to kotlin main activity for Android to mitigate crash bug on closing app

--- android/app/src/main/kotlin/club/imaginelab/geldwegwijzer/MainActivity.kt
+++ android/app/src/main/kotlin/club/imaginelab/geldwegwijzer/MainActivity.kt
@@ -6,6 +6,11 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun onDestroy() {
+        flutterEngine?.platformViewsController?.onFlutterViewDestroyed()
+        super.onDestroy()
+    }
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
