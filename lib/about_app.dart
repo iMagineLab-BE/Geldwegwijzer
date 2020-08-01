c80e4890c0c2ec674e1471271a4bac68e3d2b26e
Thomas Huybrechts <thomas.huybrechts@uantwerpen.be>
2020-08-01 21:24:37 +0200

    Fixed missing package info on web edition Bumped version to 2.0.0

--- lib/about_app.dart
+++ lib/about_app.dart
@@ -1,14 +1,27 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:geldwegwijzer/sizeconfig.dart';
 import 'package:package_info/package_info.dart';
 
 Future<void> openAboutApp(BuildContext context) async {
-  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  String appName;
+  String appVersion;
+
+  if (kIsWeb) {
+    appName = 'Geldwegwijzer';
+    appVersion = '2.0.0';
+  }
+  else {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+    appName = packageInfo.appName;
+    appVersion = packageInfo.version + ' (Build: ' + packageInfo.buildNumber + ')';
+  }
 
   showAboutDialog(
     context: context,
-    applicationName: packageInfo.appName,
-    applicationVersion: packageInfo.version + ' (Build: ' + packageInfo.buildNumber + ')',
+    applicationName: appName,
+    applicationVersion: appVersion,
     applicationLegalese: 'Copyright © 2017-' + DateTime.now().year.toString() + ' iMagineLab - All rights reserved.',
     applicationIcon: Image.asset('assets/icon.png', width: SizeConfig.blockSizeVertical * 11.0)
   );
