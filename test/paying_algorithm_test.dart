5f45061f2a79447b4334f7541f4998cd6049ac50
Jan De Laet <jan.delaet@hotmail.com>
2020-08-06 00:46:49 +0200

    Added unit tests for paying algorithm.

---
+++ test/paying_algorithm_test.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:geldwegwijzer/app_data.dart';
+import 'package:geldwegwijzer/paying.dart';
+
+void main() {
+  group('Paying Algorithm', () {
+    test('Test 1 - exact amount', () {
+      var moneyInPocket =
+          appData.currentMoney = initMoneyMap(); // cleanup method
+      moneyInPocket[describeEnum(Coin.n1Euro)] = 3;
+      moneyInPocket[describeEnum(Coin.n2Euro)] = 3;
+      double moneyToPay = 5.00;
+      print('Money to pay: ' + euroFormatter.format(moneyToPay));
+
+      var splitMoney = PayingState().calculate(moneyInPocket, moneyToPay);
+
+      print('Total split money to pay: ' +
+          euroFormatter.format(calculateIntegerCoinsValue(splitMoney) / 100));
+      print('Total money in pocket left: ' +
+          euroFormatter
+              .format(calculateIntegerCoinsValue(moneyInPocket) / 100));
+
+      expect(splitMoney[describeEnum(Coin.n1Euro)], 1); // ideal: 1
+      expect(splitMoney[describeEnum(Coin.n2Euro)], 2); // ideal: 2
+
+      expect(moneyInPocket[describeEnum(Coin.n1Euro)], 2); // ideal: 2
+      expect(moneyInPocket[describeEnum(Coin.n2Euro)], 1); // ideal: 1
+    });
+
+    test('Test 2 - not exact amount', () {
+      var moneyInPocket =
+          appData.currentMoney = initMoneyMap(); // cleanup method
+      moneyInPocket[describeEnum(Coin.n1Euro)] = 3;
+      moneyInPocket[describeEnum(Coin.n2Euro)] = 3;
+      double moneyToPay = 5.55;
+      print('Money to pay: ' + euroFormatter.format(moneyToPay));
+
+      var splitMoney = PayingState().calculate(moneyInPocket, moneyToPay);
+
+      print('Total split money to pay: ' +
+          euroFormatter.format(calculateIntegerCoinsValue(splitMoney) / 100));
+      print('Total money in pocket left: ' +
+          euroFormatter
+              .format(calculateIntegerCoinsValue(moneyInPocket) / 100));
+
+      expect(splitMoney[describeEnum(Coin.n1Euro)], 2); // ideal: 0
+      expect(splitMoney[describeEnum(Coin.n2Euro)], 2); // ideal: 3
+
+      expect(moneyInPocket[describeEnum(Coin.n1Euro)], 1); // ideal: 3
+      expect(moneyInPocket[describeEnum(Coin.n2Euro)], 1); // ideal: 0
+    });
+
+    test('Test 3 - not exact amount', () {
+      var moneyInPocket =
+          appData.currentMoney = initMoneyMap(); // cleanup method
+      moneyInPocket[describeEnum(Coin.n2Euro)] = 1;
+      moneyInPocket[describeEnum(Coin.n5Euro)] = 1;
+      moneyInPocket[describeEnum(Coin.n100Euro)] = 1;
+      double moneyToPay = 101.00;
+      print('Money to pay: ' + euroFormatter.format(moneyToPay));
+
+      var splitMoney = PayingState().calculate(moneyInPocket, moneyToPay);
+
+      print('Total split money to pay: ' +
+          euroFormatter.format(calculateIntegerCoinsValue(splitMoney) / 100));
+      print('Total money in pocket left: ' +
+          euroFormatter
+              .format(calculateIntegerCoinsValue(moneyInPocket) / 100));
+
+      expect(splitMoney[describeEnum(Coin.n2Euro)], 1); // ideal: 1
+      expect(splitMoney[describeEnum(Coin.n5Euro)], 0); // ideal: 0
+      expect(splitMoney[describeEnum(Coin.n100Euro)], 1); // ideal: 1
+
+      expect(moneyInPocket[describeEnum(Coin.n2Euro)], 0); // ideal: 0
+      expect(moneyInPocket[describeEnum(Coin.n5Euro)], 1); // ideal: 1
+      expect(moneyInPocket[describeEnum(Coin.n100Euro)], 0); // ideal: 0
+    });
+
+    test('Test 4 - not exact amount', () {
+      var moneyInPocket =
+          appData.currentMoney = initMoneyMap(); // cleanup method
+      moneyInPocket[describeEnum(Coin.n2Euro)] = 1;
+      moneyInPocket[describeEnum(Coin.n5Euro)] = 1;
+      moneyInPocket[describeEnum(Coin.n100Euro)] = 2;
+      double moneyToPay = 108.00;
+      print('Money to pay: ' + euroFormatter.format(moneyToPay));
+
+      var splitMoney = PayingState().calculate(moneyInPocket, moneyToPay);
+
+      print('Total split money to pay: ' +
+          euroFormatter.format(calculateIntegerCoinsValue(splitMoney) / 100));
+      print('Total money in pocket left: ' +
+          euroFormatter
+              .format(calculateIntegerCoinsValue(moneyInPocket) / 100));
+
+      expect(splitMoney[describeEnum(Coin.n2Euro)], 1); // ideal: 0
+      expect(splitMoney[describeEnum(Coin.n5Euro)], 1); // ideal: 0
+      expect(splitMoney[describeEnum(Coin.n100Euro)], 2); // ideal: 2
+
+      expect(moneyInPocket[describeEnum(Coin.n2Euro)], 0); // ideal: 1
+      expect(moneyInPocket[describeEnum(Coin.n5Euro)], 0); // ideal: 1
+      expect(moneyInPocket[describeEnum(Coin.n100Euro)], 0); // ideal: 0
+    });
+  });
+}
