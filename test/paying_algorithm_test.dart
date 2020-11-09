7154f57f920313611a2ab4764c26e2c548568ef2
Jan De Laet <jan.delaet@hotmail.com>
2020-11-10 00:28:27 +0100

    Added new calculate payment algorithm, it keeps track of the overage and does this recursively, prioritizes lower amount of bills, but threshold is set to never overage more than half of the amount (needs finetuning). Renamed the old one to calculate2 (can later be removed). Adapted unit tests. Todo: fix asynchronous gap when all tests are together, possibly mock the appdata.

--- test/paying_algorithm_test.dart
+++ test/paying_algorithm_test.dart
@@ -5,6 +5,8 @@ import 'package:geldwegwijzer/paying.dart';
 
 void main() {
   group('Paying Algorithm', () {
+    var payingState = PayingState();
+
     test('Test 1 - exact amount', () {
       var moneyInPocket =
           appData.currentMoney = initMoneyMap(); // cleanup method
@@ -13,7 +15,7 @@ void main() {
       double moneyToPay = 5.00;
       print('Money to pay: ' + euroFormatter.format(moneyToPay));
 
-      var splitMoney = PayingState().calculate(moneyInPocket, moneyToPay);
+      var splitMoney = payingState.calculate(moneyInPocket, moneyToPay);
 
       print('Total split money to pay: ' +
           euroFormatter.format(calculateIntegerCoinsValue(splitMoney) / 100));
@@ -36,7 +38,7 @@ void main() {
       double moneyToPay = 5.55;
       print('Money to pay: ' + euroFormatter.format(moneyToPay));
 
-      var splitMoney = PayingState().calculate(moneyInPocket, moneyToPay);
+      var splitMoney = payingState.calculate(moneyInPocket, moneyToPay);
 
       print('Total split money to pay: ' +
           euroFormatter.format(calculateIntegerCoinsValue(splitMoney) / 100));
@@ -44,11 +46,11 @@ void main() {
           euroFormatter
               .format(calculateIntegerCoinsValue(moneyInPocket) / 100));
 
-      expect(splitMoney[describeEnum(Coin.n1Euro)], 2); // ideal: 0
-      expect(splitMoney[describeEnum(Coin.n2Euro)], 2); // ideal: 3
+      expect(splitMoney[describeEnum(Coin.n1Euro)], 0); // ideal: 0
+      expect(splitMoney[describeEnum(Coin.n2Euro)], 3); // ideal: 3
 
-      expect(moneyInPocket[describeEnum(Coin.n1Euro)], 1); // ideal: 3
-      expect(moneyInPocket[describeEnum(Coin.n2Euro)], 1); // ideal: 0
+      expect(moneyInPocket[describeEnum(Coin.n1Euro)], 3); // ideal: 3
+      expect(moneyInPocket[describeEnum(Coin.n2Euro)], 0); // ideal: 0
     });
 
     test('Test 3 - not exact amount', () {
@@ -60,7 +62,7 @@ void main() {
       double moneyToPay = 101.00;
       print('Money to pay: ' + euroFormatter.format(moneyToPay));
 
-      var splitMoney = PayingState().calculate(moneyInPocket, moneyToPay);
+      var splitMoney = payingState.calculate(moneyInPocket, moneyToPay);
 
       print('Total split money to pay: ' +
           euroFormatter.format(calculateIntegerCoinsValue(splitMoney) / 100));
@@ -86,7 +88,7 @@ void main() {
       double moneyToPay = 108.00;
       print('Money to pay: ' + euroFormatter.format(moneyToPay));
 
-      var splitMoney = PayingState().calculate(moneyInPocket, moneyToPay);
+      var splitMoney = payingState.calculate(moneyInPocket, moneyToPay);
 
       print('Total split money to pay: ' +
           euroFormatter.format(calculateIntegerCoinsValue(splitMoney) / 100));
@@ -94,12 +96,12 @@ void main() {
           euroFormatter
               .format(calculateIntegerCoinsValue(moneyInPocket) / 100));
 
-      expect(splitMoney[describeEnum(Coin.n2Euro)], 1); // ideal: 0
-      expect(splitMoney[describeEnum(Coin.n5Euro)], 1); // ideal: 0
+      expect(splitMoney[describeEnum(Coin.n2Euro)], 0); // ideal: 0
+      expect(splitMoney[describeEnum(Coin.n5Euro)], 0); // ideal: 0
       expect(splitMoney[describeEnum(Coin.n100Euro)], 2); // ideal: 2
 
-      expect(moneyInPocket[describeEnum(Coin.n2Euro)], 0); // ideal: 1
-      expect(moneyInPocket[describeEnum(Coin.n5Euro)], 0); // ideal: 1
+      expect(moneyInPocket[describeEnum(Coin.n2Euro)], 1); // ideal: 1
+      expect(moneyInPocket[describeEnum(Coin.n5Euro)], 1); // ideal: 1
       expect(moneyInPocket[describeEnum(Coin.n100Euro)], 0); // ideal: 0
     });
   });
